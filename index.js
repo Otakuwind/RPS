f9cea2ec99e5e5789d3340a339eae74f228031e8
Edgar Nwajei <edgarlinked@gmail.com>
2023-05-15 16:07:49 +0100

    added eventlistener to buttons

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const player = 0;
 const computer= 0;
 const playerscore_span =document.getElementById('playerscore');
-const computer_span =document.getElementById('computerscore');
+const computer_span = document.getElementById('computerscore');
 const score_div = document.querySelector('.score');
 const result_div =document.querySelector('.result');
 
+const rock_div = document.getElementById('rock');
+const paper_div = document.getElementById('paper');
+const scissors_div = document.getElementById('scissors');
 
+
+rock_div.addEventListener('click', function(){console.log('rock');}
+)
+
+
+paper_div.addEventListener('click', function(){console.log('paper');}
+)
+
+
+scissors_div.addEventListener('click', function(){console.log('scissors');}
+)
