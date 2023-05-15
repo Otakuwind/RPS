dd62b38b76bef5f36f48367823480e6ce6c26885
Edgar Nwajei <edgarlinked@gmail.com>
2023-05-15 15:40:09 +0100

    declaring constants

---
+++ index.js
@@ -0,0 +1,8 @@
+const player = 0;
+const computer= 0;
+const playerscore_span =document.getElementById('playerscore');
+const computer_span =document.getElementById('computerscore');
+const score_div = document.querySelector('.score');
+const result_div =document.querySelector('.result');
+
+
